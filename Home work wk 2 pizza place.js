f39ee46eba60300e8e63f51f7a9db6c214cf93af
Colbin Hubbard <colbinjustin@gmail.com>
2022-06-20 12:58:07 -0500

    Commiting week 2 hw and new read me

---
+++ Home work wk 2 pizza place.js
@@ -0,0 +1,15 @@
+let pizzaPlace = "Samson's pizzaria"
+let numberOfToppings = 10
+//console.log(typeof numberOfToppings)
+//console.log(typeof pizzaPlace)
+//sentence about pizzaria
+console.log("Welcome to" + pizzaPlace + " located in St. Louis" + " we offer" + ( numberOfToppings) + " type of toppings for our pizza")
+
+if (numberOfToppings <= 10) {
+    console.log("Quality, not quantity.");
+} else (numberOfToppings >= 10){
+    console.log("A whole lot of pizza");
+}
+
+
+
