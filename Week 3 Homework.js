2e7cb3161429da62e98d68fb6e6a580eb68c816a
Colbin Hubbard <colbinjustin@gmail.com>
2022-06-27 13:17:54 -0500

    what i have for hw

--- Week 3 Homework.js
+++ Week 3 Homework.js
@@ -1,5 +1,6 @@
 // #1 Create an array of pizzaToppings with at least four different toppings
 const pizzaToppings = ["Spinach", "Tomatoes", "Black Olives", "Mushrooms", "Anchovies"];
+
 function greetCustomer() {
   console.log(" Welcome to Pizza house, our toppings are ")
 }
@@ -8,20 +9,19 @@ greetCustomer()
 // then informs them of the available toppings by looping over pizzaToppings
 
 for (let topping of pizzaToppings) {
-  console.log(topping)
+  console.log(topping);
 }
 // #3 Create a getPizzaOrder function that has the parameters size, crust,
 // and an indefinite amount of toppings as inputs. Prints the order,
 // i.e. "One large thick crust pizza with x, y, z, ... coming up!"
 // outputs a list with the size, crust, and toppings
-//let pizzaSize = 'Large'
-//let pizzaCrust = 'cheese stuffed'
-//let repeatOrder;
-function getPizzaOrder( "x", "y", "z"){
-  console.log("Large =", "x");
-  console.log("cheese stuffed crust =", "y");
-  console.log("four toppings =", "z")
-  return x + y + z
+
+function getPizzaOrder( x, y, z){
+  console.log(`Large =`, x);
+  console.log(`cheese stuffed crust =`, y);
+  console.log(`four toppings =`, z);
+  return `One ${x}, ${y}, pizza with ${z}, coming up`
 }
-getPizzaOrder(x, y, z)
-console.log("One large thick crust pizza with x, y, z, ... coming up!")
+console.log(getPizzaOrder);
+getPizzaOrder();
+
