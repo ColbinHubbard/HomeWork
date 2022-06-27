768b00456fb6d93ee83f697aa3b600d0a87fefb2
Colbin Hubbard <colbinjustin@gmail.com>
2022-06-27 09:20:41 -0500

    I couldnt finish the homework wk3

--- Week 3 Homework.js
+++ Week 3 Homework.js
@@ -1,24 +1,27 @@
 // #1 Create an array of pizzaToppings with at least four different toppings
+const pizzaToppings = ["Spinach", "Tomatoes", "Black Olives", "Mushrooms", "Anchovies"];
 function greetCustomer() {
   console.log(" Welcome to Pizza house, our toppings are ")
 }
 greetCustomer()
 // #2 Create a greetCustomer function that prints a message that welcomes a guest,
 // then informs them of the available toppings by looping over pizzaToppings
-const pizzaToppings = ["Spinach", "Tomatoes", "Black Olives", "Mushrooms", "Anchovies"];
-for (let name of pizzaToppings) {
-  console.log(greetCustomer + pizzaToppings)
+
+for (let topping of pizzaToppings) {
+  console.log(topping)
 }
-/ #3 Create a getPizzaOrder function that has the parameters size, crust,
+// #3 Create a getPizzaOrder function that has the parameters size, crust,
 // and an indefinite amount of toppings as inputs. Prints the order,
 // i.e. "One large thick crust pizza with x, y, z, ... coming up!"
 // outputs a list with the size, crust, and toppings
-let pizzaSize = "Large"
-let pizzaCrust = "cheese stuffed"
-let repeatOrder;
-function myPizzaOrder() {
-  repeatOrder = 'One ${pizzaSize} ${pizzaCrust} crust pizza with';
-  return repeatOrder;
+//let pizzaSize = 'Large'
+//let pizzaCrust = 'cheese stuffed'
+//let repeatOrder;
+function getPizzaOrder( "x", "y", "z"){
+  console.log("Large =", "x");
+  console.log("cheese stuffed crust =", "y");
+  console.log("four toppings =", "z")
+  return x + y + z
 }
-myPizzaOrder()
-console.log(repeatOrder + pizzaToppings + " coming right up! ")
+getPizzaOrder(x, y, z)
+console.log("One large thick crust pizza with x, y, z, ... coming up!")
